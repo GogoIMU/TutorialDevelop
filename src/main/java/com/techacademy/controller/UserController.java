97d301a461181c4a50c6e0774c7d93fbc423ff7d
GogoIMU <meiyou358@gmail.com>
2024-08-08 16:15:43 +0900

    2024/08/08 課題用作成

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -41,7 +41,6 @@ public class UserController {
         return "user/register";
     }
 
-    // ----- 変更ここから -----
     /** User登録処理 */
     @PostMapping("/register")
     public String postRegister(@Validated User user, BindingResult res, Model model) {
@@ -54,25 +53,39 @@ public class UserController {
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // ----- 変更ここまで -----
 
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
     public String getUser(@PathVariable("id") Integer id, Model model) {
+        User user;
+        if (id != null) {
+        // idがnullではない場合、サービスからUserをセット
+            user = service.getUser(id);
+        } else {
+        // idがnullの場合、新しいUserを作成
+            user = new User();
+        }
         // Modelに登録
-        model.addAttribute("user", service.getUser(id));
+        model.addAttribute("user", user);
+
         // User更新画面に遷移
         return "user/update";
     }
 
     /** User更新処理 */
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
+    public String postUser(@Validated User user, BindingResult res, Model model) {
+        if (res.hasErrors()) {
+            // エラーあり
+            model.addAttribute("user", user);
+            return "user/update";
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
+
 
     /** User削除処理 */
     @PostMapping(path="list", params="deleteRun")
--- src/test/java/com/techacademy/controller/UserControllerTest.java
+++ src/test/java/com/techacademy/controller/UserControllerTest.java
@@ -7,6 +7,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -61,4 +64,38 @@ class UserControllerTest {
         assertEquals("キラメキ太郎", user.getName());
     }
 
-}
+        // ----- 追加：getList()メソッドに対するテスト -----
+        @Test
+        @DisplayName("userlist")
+        @WithMockUser
+        void testGetList() throws Exception {
+            // HTTPリクエストに対するレスポンスの検証
+            MvcResult result = mockMvc.perform(get("/user/list")) // URLにアクセス
+                    .andExpect(status().isOk()) // ステータスを確認
+                    .andExpect(model().attributeExists("userlist")) // Modelの内容を確認
+                    .andExpect(model().hasNoErrors()) // Modelのエラー有無の確認
+                    .andExpect(view().name("user/list")) // viewの確認
+                    .andReturn(); // 内容の取得
+
+            // userの検証
+            // Modelからuserlistを取り出す
+            List<User> userList = (ArrayList<User>)
+            result.getModelAndView().getModel().get("userlist");
+            assertEquals(3, userList.size());
+
+            // 各Userの検証
+            User user1 = userList.get(0);
+            assertEquals(1, user1.getId());
+            assertEquals("キラメキ太郎", user1.getName());
+
+            User user2 = userList.get(1);
+            assertEquals(2, user2.getId());
+            assertEquals("キラメキ次郎", user2.getName());
+
+            User user3 = userList.get(2);
+            assertEquals(3, user3.getId());
+            assertEquals("キラメキ花子", user3.getName());
+        }
+
+    }
+
